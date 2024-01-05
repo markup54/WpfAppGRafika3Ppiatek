b9c467c45f9b28535538a0e77fc02b84b18d3d8c
markup54 <marzenakupka@tlen.pl>
2024-01-05 09:17:57 +0100

    Dodaj pliki projektów.

---
+++ WpfAppGRafika3Ppiatek/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WpfAppGRafika3Ppiatek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<BitmapImage> Images { get; set; }
+        public int Aktualny { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Aktualny = 0;
+            przygotujDane();
+        }
+
+        private void przygotujDane()
+        {
+            Images = new List<BitmapImage>();
+            Images.Add(new BitmapImage(new Uri("rys1.jpg", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("rys2.jpg", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("rys3.jpg", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("rys4.jpg", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("rys5.jpg", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("rys6.jpg", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("rys7.jpg", UriKind.Relative)));
+            Images.Add(new BitmapImage(new Uri("rys8.jpg", UriKind.Relative)));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Aktualny--;
+            if(Aktualny < 0)
+                Aktualny = Images.Count - 1;
+            obrazek.Source = Images[Aktualny];
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Aktualny++;
+            if (Aktualny == Images.Count)
+                Aktualny = 0;
+            obrazek.Source = Images[Aktualny];
+
+        }
+
+        private void nr_obrazka_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int nr = (int)nr_obrazka.SelectedIndex;
+            MessageBox.Show(nr.ToString());
+            obrazek_cb.Source = Images[nr];
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton radio = (RadioButton)sender;
+            //sender as RadioButton;
+
+            obrazek_radio.Source = new BitmapImage(new Uri("rys1.jpg", UriKind.Relative));
+        }
+    }
+}
---
+++ WpfAppGRafika3Ppiatek/Menu_ObrazkiWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppGRafika3Ppiatek
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Menu_ObrazkiWindow.xaml
+    /// </summary>
+    public partial class Menu_ObrazkiWindow : Window
+    {
+        public Menu_ObrazkiWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            obrazek_menu.Source = new BitmapImage(new Uri("rys2.jpg", UriKind.Relative));
+        }
+    }
+}
